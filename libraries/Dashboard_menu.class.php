e663f41b1cea306bc372509dd04dc3f3b9c4c0b6
David Miguel Gómez Macías <davidgomezmacias@gmail.com>
2020-12-08 12:46:14 -0500

    Init

---
+++ libraries/Dashboard_menu.class.php
@@ -0,0 +1,28 @@
+<?php
+defined('_EXEC') or die;
+
+class Dashboard_menu
+{
+    static public function main_menu() : array
+    {
+        $menu = [];
+
+        $menu[] = [
+            'name' => 'General',
+            'url' => 'index.php',
+            'icon' => 'dripicons-home'
+        ];
+
+        if ( in_array('{blog_read}', Session::get_value('session_permissions')) )
+        {
+            $menu[] = [
+                'name' => 'Blog',
+                'url' => 'index.php?c=blog',
+                'taget' => '_blank',
+                'icon' => 'dripicons-blog'
+            ];
+        }
+
+        return $menu;
+    }
+}
